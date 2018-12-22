1589eaa804dfbe29790adc577a4ee4762df0b9a2
独行工匠 <515235416@qq.com>
2018-12-22 18:16:24 +0800

    添加轮播图和tab组件

---
+++ components/lunbotu.vue
@@ -0,0 +1,43 @@
+<!-- 轮播图 -->
+<template>
+
+	<swiper :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration" circular="true"
+	 :indicator-active-color="active_color" :indicator-color="indicator">
+		<block v-for="item in imgUrls">
+			<swiper-item>
+				<image :src="item" class="slide-image" width="355" height="150" />
+			</swiper-item>
+		</block>
+	</swiper>
+
+</template>
+<script>
+	export default {
+		props:{
+			imgUrls:""
+		},
+		data() {
+			return {
+				indicatorDots: true, //是否显示点点
+				autoplay: true, //自动播放
+				active_color: "#fff", //点选中的颜色
+				indicator: "#B4B4B4", //点的颜色
+				interval: 5000,
+				duration: 1000,
+			}
+		},
+		components: {},
+		methods: {},
+		mounted() {},
+	}
+</script>
+<style scoped>
+	.slide-image {
+		width: 100%;
+		height: 100%;
+	}
+
+	swiper {
+		height: 100%
+	}
+</style>
---
+++ components/tab_qh.vue
@@ -0,0 +1,49 @@
+<!-- tab切换 -->
+<template>
+	<view class="box cen dsf_ddrertxc bbm" :class="is_sdf==1?'act':''">
+		<view class="box_a df_fd_sdrert" :class="sd.cls" v-for="sd in sd_dfg" @click="sd_sddf(sd)">
+			<text class="pd fz28 z9">{{sd.name}}</text>
+		</view>
+	</view>
+</template>
+<script>
+	export default {
+		props:{
+			sd_dfg:'',
+			is_sdf:""
+		},
+		data() {
+			return {}
+		},
+		components: {},
+		methods: {
+			sd_sddf(sd){
+				this.sd_dfg.map(a=>{
+					a.cls=""
+				})
+				sd.cls="act"
+			}
+		},
+		mounted() {},
+	}
+</script>
+<style scoped>
+	.dsf_ddrertxc{
+		position: fixed;
+		left: 0;
+		top: 0;
+		z-index: 10000;
+		background: #fff;
+		line-height: 100upx;
+		height: 100upx;
+		width: 100%;
+	}
+	.dsf_ddrertxc.act{
+		position: relative;
+	}
+	.df_fd_sdrert.act text{
+		padding-bottom: 26upx;
+		color:#459FF7 ;
+		border-bottom: 1px solid #459FF7;
+	}
+</style>
